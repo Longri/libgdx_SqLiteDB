9f9ff132e9d26f9102985627c2d359af9f43f9fe
Longri <develop@longri.de>
2015-09-13 10:51:14 +0200

    + add libGdx UI with LibGdx-Setup

---
+++ android/src/de/cb/sqlite/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.cb.sqlite.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.cb.sqlite.MyGdxTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxTest(), config);
+	}
+}
---
+++ desktop/src/de/cb/sqlite/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.cb.sqlite.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.cb.sqlite.MyGdxTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxTest(), config);
+	}
+}
