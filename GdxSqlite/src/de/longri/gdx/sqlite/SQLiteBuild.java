d9fbe91b77dcfb5ab166c48deb3723b6868f4f28
Longri <develop@longri.de>
2017-12-20 15:11:29 +0100

    compile funktion for Mac and iOS

--- GdxSqlite/src/de/longri/gdx/sqlite/SQLiteBuild.java
+++ GdxSqlite/src/de/longri/gdx/sqlite/SQLiteBuild.java
@@ -1,10 +1,13 @@
 package de.longri.gdx.sqlite;
 
 
-import com.badlogic.gdx.jnigen.AntScriptGenerator;
-import com.badlogic.gdx.jnigen.BuildConfig;
-import com.badlogic.gdx.jnigen.BuildTarget;
-import com.badlogic.gdx.jnigen.NativeCodeGenerator;
+import com.badlogic.gdx.jnigen.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * Created by Longri on 18.12.2017.
@@ -12,23 +15,70 @@ import com.badlogic.gdx.jnigen.NativeCodeGenerator;
 public class SQLiteBuild {
 
     public native void add(float[] values, int offset, int numElements, float value); /*
-        for(int i = offset; i,numElements; i++){
-            values[i]+= value;
+        for(int i = offset; i < numElements; i++){
+            values[i]+= value + value;
         }
     */
 
     public static void main(String[] args) throws Exception {
-        // generate native code
-        new NativeCodeGenerator().generate("src", "build/classes/main", "jni");
+
+        if (true) {
+            // generate native code
+            new NativeCodeGenerator().generate("src", "build/classes/main", "jni");
 
 
-        //generate build scripts
-        BuildTarget win64 = BuildTarget.newDefaultTarget(BuildTarget.TargetOs.Windows, true);
-        win64.compilerPrefix = "";
-        win64.compilerSuffix = ".exe";
+            //generate build scripts
+            BuildTarget win64 = BuildTarget.newDefaultTarget(BuildTarget.TargetOs.Windows, true);
+            win64.compilerPrefix = "";
+            win64.compilerSuffix = "";
 
-        BuildConfig config = new BuildConfig("GdxSqlite");
-        new AntScriptGenerator().generate(config, win64);
+            BuildTarget win32 = BuildTarget.newDefaultTarget(BuildTarget.TargetOs.Windows, false);
+            win32.compilerPrefix = "";
+            win32.compilerSuffix = "";
+
+            BuildTarget mac64 = BuildTarget.newDefaultTarget(BuildTarget.TargetOs.MacOsX, true);
+            mac64.compilerPrefix = "";
+            mac64.compilerSuffix = "";
+
+            BuildTarget ios32 = BuildTarget.newDefaultTarget(BuildTarget.TargetOs.IOS, false);
+            ios32.compilerPrefix = "";
+            ios32.compilerSuffix = "";
+
+
+            BuildConfig config = new BuildConfig("GdxSqlite");
+            new AntScriptGenerator().generate(config, mac64, ios32);
+
+            String path = new FileDescriptor("").file().getAbsolutePath();
+
+            BuildExecutor.executeAnt("jni/build.xml", "-v");
+        }
+
+        File path = new FileDescriptor("jni").file().getAbsoluteFile();
+
+
+        Process exec = new ProcessBuilder("ant", "-f", "build-macosx64.xml", "-v").directory(path).start();
+        try (BufferedReader out = new BufferedReader(new InputStreamReader(exec.getInputStream()))) {
+            out.lines().collect(Collectors.toList()).forEach(System.out::println);
+        }
+
+        exec = new ProcessBuilder("ant", "-f", "build-ios32.xml", "-v").directory(path).start();
+        try (BufferedReader out = new BufferedReader(new InputStreamReader(exec.getInputStream()))) {
+            out.lines().collect(Collectors.toList()).forEach(System.out::println);
+        }
+
+
+        exec = new ProcessBuilder("ant", "-v").directory(path).start();
+        try (BufferedReader out = new BufferedReader(new InputStreamReader(exec.getInputStream()))) {
+            out.lines().collect(Collectors.toList()).forEach(System.out::println);
+        }
+
+
+        new JniGenSharedLibraryLoader("libs/GdxSqlite-natives.jar").load("GdxSqlite");
+
+        float[] values = new float[]{1, 2, 3, 4, 5};
+        new SQLiteBuild().add(values, 0, 5, 5);
+        System.out.println(Arrays.toString(values));
+
 
     }
 }
