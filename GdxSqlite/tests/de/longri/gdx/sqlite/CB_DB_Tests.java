f212b191cbefddbafad5a77523055baa3c58dd00
Longri <www.longri.de@googlemail.com>
2018-01-11 13:22:52 +0100

    fix CB_DB_Tests load test DB from Asset

--- GdxSqlite/tests/de/longri/gdx/sqlite/CB_DB_Tests.java
+++ GdxSqlite/tests/de/longri/gdx/sqlite/CB_DB_Tests.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright (C) 2018 team-cachebox.de
+ *
+ * Licensed under the : GNU General  License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.longri.gdx.sqlite;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import org.junit.jupiter.api.AfterAll;
@@ -28,6 +44,23 @@ public class CB_DB_Tests {
         //load natives
         TestUtils.loadSharedLib("GdxSqlite");
         testFolder.mkdirs();
+
+        FileHandle asset = Gdx.files.internal("GdxSqlite/testResources/cachebox.db3");
+        if (asset.exists()&& (Gdx.app.getType()== Application.ApplicationType.Android || Gdx.app.getType()== Application.ApplicationType.iOS)) {
+
+            //delete old tests
+            testFolder.deleteDirectory();
+            testFolder.mkdirs();
+
+            // copy to test folder
+            asset.copyTo(testFolder);
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- GdxSqlite/tests/de/longri/gdx/sqlite/TestUtils.java
+++ GdxSqlite/tests/de/longri/gdx/sqlite/TestUtils.java
@@ -15,8 +15,9 @@
  */
 package de.longri.gdx.sqlite;
 
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.backends.lwjgl.LwjglFiles;
+import com.badlogic.gdx.utils.Clipboard;
 import com.badlogic.gdx.utils.SharedLibraryLoader;
 
 /**
@@ -26,6 +27,137 @@ public class TestUtils {
     public static void initialGdx() {
         if (Gdx.files != null) return;
         Gdx.files = new LwjglFiles();
+        Gdx.app = new Application() {
+            @Override
+            public ApplicationListener getApplicationListener() {
+                return null;
+            }
+
+            @Override
+            public Graphics getGraphics() {
+                return null;
+            }
+
+            @Override
+            public Audio getAudio() {
+                return null;
+            }
+
+            @Override
+            public Input getInput() {
+                return null;
+            }
+
+            @Override
+            public Files getFiles() {
+                return null;
+            }
+
+            @Override
+            public Net getNet() {
+                return null;
+            }
+
+            @Override
+            public void log(String s, String s1) {
+
+            }
+
+            @Override
+            public void log(String s, String s1, Throwable throwable) {
+
+            }
+
+            @Override
+            public void error(String s, String s1) {
+
+            }
+
+            @Override
+            public void error(String s, String s1, Throwable throwable) {
+
+            }
+
+            @Override
+            public void debug(String s, String s1) {
+
+            }
+
+            @Override
+            public void debug(String s, String s1, Throwable throwable) {
+
+            }
+
+            @Override
+            public void setLogLevel(int i) {
+
+            }
+
+            @Override
+            public int getLogLevel() {
+                return 0;
+            }
+
+            @Override
+            public void setApplicationLogger(ApplicationLogger applicationLogger) {
+
+            }
+
+            @Override
+            public ApplicationLogger getApplicationLogger() {
+                return null;
+            }
+
+            @Override
+            public ApplicationType getType() {
+                return ApplicationType.HeadlessDesktop;
+            }
+
+            @Override
+            public int getVersion() {
+                return 0;
+            }
+
+            @Override
+            public long getJavaHeap() {
+                return 0;
+            }
+
+            @Override
+            public long getNativeHeap() {
+                return 0;
+            }
+
+            @Override
+            public Preferences getPreferences(String s) {
+                return null;
+            }
+
+            @Override
+            public Clipboard getClipboard() {
+                return null;
+            }
+
+            @Override
+            public void postRunnable(Runnable runnable) {
+
+            }
+
+            @Override
+            public void exit() {
+
+            }
+
+            @Override
+            public void addLifecycleListener(LifecycleListener lifecycleListener) {
+
+            }
+
+            @Override
+            public void removeLifecycleListener(LifecycleListener lifecycleListener) {
+
+            }
+        };
     }
 
     public static void loadSharedLib(String gdxSqlite) {
